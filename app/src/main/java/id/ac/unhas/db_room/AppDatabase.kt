a00fcbf2027b6de48232f6e293e788d7a3802d43
Ahmad Affandy <pancozgnoxz@gmail.com>
2021-05-26 16:58:58 +0800

    @database

---
+++ app/src/main/java/id/ac/unhas/db_room/AppDatabase.kt
@@ -0,0 +1,30 @@
+package id.ac.unhas.db_room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], exportSchema = false, version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+
+        private const val DB_NAME = "NOTE_DB"
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase? {
+            if (instance == null) {
+                synchronized(AppDatabase::class) {
+                    instance = Room
+                            .databaseBuilder(
+                                    context,
+                                    AppDatabase::class.java,
+                                    DB_NAME
+                            )
+                            .build()
+                }
+            }
+            return instance
+        }
+    }
+}
