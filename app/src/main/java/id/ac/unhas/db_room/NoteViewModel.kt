3a43a25fdb85475b0fbe09b90001d82f1f60aa02
Ahmad Affandy <pancozgnoxz@gmail.com>
2021-05-26 17:08:20 +0800

    @ViewModel

---
+++ app/src/main/java/id/ac/unhas/db_room/NoteViewModel.kt
@@ -0,0 +1,24 @@
+package id.ac.unhas.db_room
+
+import androidx.lifecycle.LiveData
+
+class NoteViewModel {
+    private var noteRepository = NoteRepository(application)
+    private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+    fun insertNote(note: Note) {
+        noteRepository.insert(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun deleteNote(note: Note) {
+        noteRepository.delete(note)
+    }
+
+    fun updateNote(note: Note) {
+        noteRepository.update(note)
+    }
+}
